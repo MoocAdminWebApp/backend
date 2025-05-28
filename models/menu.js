d40a2a5e5fd584603b22926046e8561646773fe4
Jessie Zhou <z.jessie.jh@gmail.com>
2025-05-28 20:08:51 +1000

    add menu model and migration files
    sync: rebase feature/menu branch onto dev

--- models/menu.js
+++ models/menu.js
@@ -47,7 +47,7 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
         allowNull: true,
       },
-      hidden: {
+      hiddle: {
         type: DataTypes.BOOLEAN,
         allowNull: true,
       },
