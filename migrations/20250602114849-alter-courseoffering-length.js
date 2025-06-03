e806696094bab860b153ea9a172ed0ee12a6a21f
David <llttww0912@gmail.com>
2025-06-03 15:00:37 +1000

    add enum-add column length-CRUD-test

---
+++ migrations/20250602114849-alter-courseoffering-length.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.changeColumn('CourseOfferings', 'courseName', {
+      type: Sequelize.STRING(50),
+      allowNull: false,
+    });
+    await queryInterface.changeColumn('CourseOfferings', 'teacherName', {
+      type: Sequelize.STRING(40),
+      allowNull: false,
+    });
+    await queryInterface.changeColumn('CourseOfferings', 'location', {
+      type: Sequelize.STRING(50),
+      allowNull: true,
+    });
+    await queryInterface.changeColumn('CourseOfferings', 'schedule', {
+      type: Sequelize.STRING(50),
+      allowNull: true,
+    });
+    await queryInterface.changeColumn('CourseOfferings', 'semester', {
+      type: Sequelize.STRING(40),
+      allowNull: false,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.changeColumn('CourseOfferings', 'courseName', {
+      type: Sequelize.STRING,
+      allowNull: false,
+    });
+    await queryInterface.changeColumn('CourseOfferings', 'teacherName', {
+      type: Sequelize.STRING,
+      allowNull: false,
+    });
+    await queryInterface.changeColumn('CourseOfferings', 'location', {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
+    await queryInterface.changeColumn('CourseOfferings', 'schedule', {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
+    await queryInterface.changeColumn('CourseOfferings', 'semester', {
+      type: Sequelize.STRING,
+      allowNull: false,
+    });
+  }
+};
